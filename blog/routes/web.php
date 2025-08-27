285c3b2a7a2df10bbbdaeb0bd829561d2b196271
AaronSotoPacheco <111542116+AaronSotoPacheco@users.noreply.github.com>
2025-08-27 15:29:00 -0700

    Modificacion de base de datos

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -5,8 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route ::get("/contacto",function(){
+Route::get("/contacto",function(){
     return view('contacto');
     
 });
+Route::get("/post",function(){
+    return view('post');
+    
+});
+Route::get("/about",function(){
+    return view('about');
+    
+});
+
+
 
