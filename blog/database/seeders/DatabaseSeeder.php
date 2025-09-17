48df9fde2a8e733141214d41860c068c4c6c6b65
AaronSotoPacheco <111542116+AaronSotoPacheco@users.noreply.github.com>
2025-09-17 13:44:23 -0700

    si
    s

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -3,18 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
     {
-        $this->call(users_seed::class);
-        $this->call(categories::class);
-
+        
+        $this->call(users__seed::class);
+        $this->call(categories_seed::class);
+        $this->call(PostSeeder::class);
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,22 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//ENDPOINT 
+//ENDPOINTS
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/contacto",function(){
+Route::get("/contact",function(){
     return view('contacto');
-    
 });
 Route::get("/post",function(){
     return view('post');
-    
 });
 Route::get("/about",function(){
     return view('about');
-    
 });
-
-
-
+Route::get("/dashboard",function(){
+    return view('admin.dashboard');
+});
